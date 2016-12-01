6109bb6b84e7be11c048f1ebab3611291cae41fe
Orion <orionmhall@gmail.com>
2016-12-01 11:32:26 -0500

    User.cs model added

---
+++ ImpactMap/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImpactMap.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public Guid userGuid { get; set; }
+    }
+}
