8763198c5ccb61be53d4f09202eea28b00055cc5
Orion <orionmhall@gmail.com>
2016-12-07 23:25:16 -0500

    So many changes, where to begin? InvestmentsIn/Out lists on projects populate automatically when Investment is created, also Investment index is done(?) and Projects almost is

---
+++ ImpactMap/Migrations/201612080221462_InvestmentsInOut.cs
@@ -0,0 +1,16 @@
+namespace ImpactMap.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InvestmentsInOut : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
