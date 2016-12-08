4f91230a3ef12e21d23d71968684b9746e056a47
Orion <orionmhall@gmail.com>
2016-12-07 20:19:39 -0500

    Working on view Projects page

--- ImpactMap/Models/Project.cs
+++ ImpactMap/Models/Project.cs
@@ -10,13 +10,15 @@ namespace ImpactMap.Models
     public class Project
     {
         public int ID { get; set; }
-        [Display(Name = "Project Title")]
+        [Display(Name = "Name")]
         public string name { get; set; }
         public virtual Entity entity { get; set; }
-        [Display(Name = "Project Description")]
+        [Display(Name = "Description")]
         [DataType(DataType.MultilineText)]
         public string description { get; set; }
+        [Display(Name = "Investment In")]
         public virtual Investment investmentIn { get; set; }
+        [Display(Name = "Investment Out")]
         public virtual Investment investmentOut { get; set; }
         [Display(Name = "Report")]
         public virtual Report report { get; set; }
