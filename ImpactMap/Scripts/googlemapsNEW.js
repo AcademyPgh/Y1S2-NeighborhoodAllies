3e58aff742ea658cdde5620cfa21731dfaed1780
Orion <orionmhall@gmail.com>
2016-12-15 19:28:15 -0500

    Some more map testing stuff

--- ImpactMap/Scripts/googlemapsNEW.js
+++ ImpactMap/Scripts/googlemapsNEW.js
@@ -29,7 +29,7 @@ function initialize() {
     var directionsService = new google.maps.DirectionsService();
     var directionsDisplay = new google.maps.DirectionsRenderer();
     var pittsburgh = new google.maps.LatLng(40.421796, -79.994485);
-    mapOptions = { zoom: 10, mapTypeId: google.maps.MapTypeId.ROADMAP, center: pittsburgh }
+    mapOptions = { zoom: 13, mapTypeId: google.maps.MapTypeId.ROADMAP, center: pittsburgh }
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
     directionsDisplay.setMap(map);
 
@@ -39,21 +39,21 @@ function initialize() {
 
     //    new google.maps.places.Autocomplete(input, options);
     //}
-}
+};
 
 //Handles marker placement, dynamic zoom, and on click functionality.
 function markerlocation(organizationid, address, organization, organizationabout) {
-    y = 0;
     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&organization=' + organization + '&organizationabout=' + organization + '&sensor=false', null, function (data) {
         var addresslocation = data.results[0].geometry.location;
         var latlng = new google.maps.LatLng(addresslocation.lat, addresslocation.lng);
+        linepaths.push({ lat: addresslocation.lat, lng: addresslocation.lng });
         var marker = new google.maps.Marker({
             position: latlng, //it will place marker based on the addresses, which they will be translated as geolocations.
             map: map,
             title: organization,
             animation: google.maps.Animation.DROP,
             id: organizationid
-        });
+        });        
 
         var contentString =
         '<div>' +
@@ -67,7 +67,7 @@ function markerlocation(organizationid, address, organization, organizationabout
                 '<p class="descripbox">' + organizationabout + '</p>' +
             '</div>' +
         '</div>'
-                ;
+        ;
 
         var infowindow = new google.maps.InfoWindow({
             content: contentString
@@ -80,4 +80,30 @@ function markerlocation(organizationid, address, organization, organizationabout
             map.setCenter(marker.getPosition());
         });
     });
+};
+
+function createLine() {
+    console.log("getting called");
+    var lol = [{ lat: 40.421572, lng: -79.99410619999999 }, { lat: 40.4369999, lng: -79.9970905 }, { lat: 40.4218834, lng: -79.9939074 }];
+    var lolly = linepaths.toString();
+    console.log(lolly);
+    flightPath = new google.maps.Polyline({
+        path: linepaths,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: null
+    });
+
+    //addLine();
+    console.log("bottom of function");
 }
+
+function addLine() {
+    flightPath.setMap(map);
+}
+
+function removeLine() {
+    flightPath.setMap(null);
+}
+
