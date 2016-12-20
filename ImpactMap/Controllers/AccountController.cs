e0bcbb34bcc8d1944d4e009517233b5375cf7c53
Orion <orionmhall@gmail.com>
2016-12-19 20:33:38 -0500

    Log off now redirects to login/landing page

--- ImpactMap/Controllers/AccountController.cs
+++ ImpactMap/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace ImpactMap.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
